3623b95f2cb94c995ce8e3def30518471d54cd10
Saveliy <svolskey2929@gmail.com>
2022-05-31 00:32:15 +0300

    added send images in chat

--- src/components/ContentPage/DialogsComponents/ChatWindow/ChatWindow.js
+++ src/components/ContentPage/DialogsComponents/ChatWindow/ChatWindow.js
@@ -21,16 +21,38 @@ const ChatWindow = () => {
 
     const [textarea, setTextarea] = React.useState('');
 
+    const [openInput, setOpenInput] = React.useState(false);
+    const [inputImgValue, setInputImgValue] = React.useState('');
+
+
     const handlePushNewMessage = () => {
-        const date = new Date()
-        const newMessageListRef = ref(dataBase, `activeDialogs/${currentDialog}/messages`);
-        const newMessageRef = push(newMessageListRef);
-        set(newMessageRef, {
-            messageId: newMessageRef._path.pieces_[Object.keys(newMessageRef._path.pieces_).length - 1],
-            text: textarea,
-            timestamp: date.getTime(),
-            senderName: email,
-        });
+        if (textarea) {
+            const date = new Date()
+            const newMessageListRef = ref(dataBase, `activeDialogs/${currentDialog}/messages`);
+            const newMessageRef = push(newMessageListRef);
+            set(newMessageRef, {
+                messageId: newMessageRef._path.pieces_[Object.keys(newMessageRef._path.pieces_).length - 1],
+                text: textarea,
+                timestamp: date.getTime(),
+                senderName: email,
+            });
+        }
+    }
+
+    const handlePushNewImgMessage = () => {
+        if (inputImgValue) {
+            const date = new Date()
+            const newMessageListRef = ref(dataBase, `activeDialogs/${currentDialog}/messages`);
+            const newMessageRef = push(newMessageListRef);
+            set(newMessageRef, {
+                messageId: newMessageRef._path.pieces_[Object.keys(newMessageRef._path.pieces_).length - 1],
+                imgSrc: inputImgValue,
+                timestamp: date.getTime(),
+                senderName: email,
+            });
+            setInputImgValue('')
+        }
+        setOpenInput(false)
     }
 
     return (
@@ -45,10 +67,14 @@ const ChatWindow = () => {
                     {Object.values(dialog.messages).map((item, index) => (
                             <div key={'currentDialog' + index} className={s.message}>
                                 <div className={s.text}>
-                                    {item.text}
+                                    {item.text ? item.text :
+                                        <img className={s.messageImg} src={item.imgSrc} alt='Image Error'/>}
                                 </div>
                                 <div className={s.name}>
                                     {item.senderName}
+                                </div>
+                                <div className={s.name}>
+                                    {item.timestamp}
                                 </div>
                             </div>
                         )
@@ -67,8 +93,26 @@ const ChatWindow = () => {
                             />
                             <Button onClick={handlePushNewMessage}>Отправить</Button>
                         </div>
+                        {/* выбрать из готовых */}
                         <div className={s.template}>
-                            Выбрать из готовых (in process)
+                            {openInput ? <Button onClick={handlePushNewImgMessage}>Отправить изображение</Button> :
+                                <Button onClick={() => setOpenInput(true)}>Добавить изображение</Button>}
+                            {openInput &&
+                                <Input
+                                    value={inputImgValue}
+                                    onChange={e => setInputImgValue(e.target.value)}
+                                    placeholder='Введите ссылку на изображение'
+                                    type="text"
+                                />}
+                            {/*Выберите из готовых:*/}
+                            {/*<Input list='answers'/>*/}
+                            {/*<datalist id='answers'>*/}
+                            {/*    <option value="Ke11k11" />*/}
+                            {/*    <option value="222 sadasd asd as asdasd aw olds sdf ksdf sdlf sldflsdfl sdf ldslfsdlf ldlsflsdlf" />*/}
+                            {/*    <option value="333" />*/}
+                            {/*    <option value="ke444k" />*/}
+                            {/*    <option value="ke555k" />*/}
+                            {/*</datalist>*/}
                         </div>
                     </div>
                 }
